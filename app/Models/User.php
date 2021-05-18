188cd7d921bcf6aa3f7fb0a8f8a89ed923538be0
ThomasD <t.ducugnon@gmail.com>
2021-05-18 10:44:13 +0200

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Model
+{
+    use HasFactory, Notifiable;
+
+    const USER_ROLE = 'USER';
+    const ADMIN_ROLE = 'ADMIN';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role'
+    ];
+
+}

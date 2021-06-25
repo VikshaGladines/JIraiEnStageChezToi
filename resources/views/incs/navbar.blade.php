3bbff34f350f47c763cce35e8526aa7b5e0f16b0
Ducugnon Thomas <thomas.ducugnons@gmail.com>
2021-06-25 16:32:38 +0200

    final change

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>J'irai en stage chez toi</title>
 </head>
 <body>
     <header class="container-fluid">

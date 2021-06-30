ad091fe5c3eca3fbddf1f60a3ce1919ff45e9ef6
Ducugnon Thomas <thomas.ducugnons@gmail.com>
2021-06-30 15:12:06 +0200

    design change

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,7 +9,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>J'irai en stage chez toi</title>
     <style>
